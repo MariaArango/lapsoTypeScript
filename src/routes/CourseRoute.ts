6fe691e152607321138ce4af4235a57908d1824b
maria_rodriguez_arango <mery.arango86@gmail.com>
2022-11-13 20:45:02 +0100

    rutas simplificadas

--- src/routes/CourseRoute.ts
+++ src/routes/CourseRoute.ts
@@ -4,7 +4,7 @@ import { CourseController } from "../controllers/CourseController";
 const CourseRouter = Router();
 CourseRouter.route('/').get(CourseController.getCourses);
 CourseRouter.route('/:id').get(CourseController.getCourseById);
-CourseRouter.route('/delete/:id').delete(CourseController.deleteCourse);
+CourseRouter.route('/:id').delete(CourseController.deleteCourse);
 
 
 export {CourseRouter};
--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -4,7 +4,7 @@ import { UserController } from '../controllers/UserController';
 const UserRouter = Router();
 UserRouter.route('/').get(UserController.getUsers);
 UserRouter.route('/:id').get(UserController.getUserById);
-UserRouter.route('/delete/:id').delete(UserController.deleteUser);
-UserRouter.route('/create').post(UserController.createUser);
+UserRouter.route('/:id').delete(UserController.deleteUser);
+UserRouter.route('/').post(UserController.createUser);
 
 export { UserRouter };
